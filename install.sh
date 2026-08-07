e79b18ce53ab953d784c7d4148730a815777a7b2
auxmeet <mit35826@gmail.com>
2026-08-07 13:08:29 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-echo -e "Script will install i3wm or bspwm..."
-read -p "Continue? (i3wm/bspwm): " ans
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+NC='\033[0m'
+echo -e "${YELLOW}Script will install i3wm or bspwm from my repo...${NC}"
+read -p "${YELLOW}Continue? (i3wm/bspwm): ${NC}" ans
 if [[ "$ans" == "i3wm" ]]; then
-  echo -e "Script will install i3wm dotfiles from git"
+  echo -e "${YELLOW}Script will install i3wm dotfiles from git${NC}"
   git clone https://github.com/auxmeet/i3wm-dotfiles.git
   cd i3wm-dotfiles
   chmod +x onestep.sh
@@ -11,7 +15,7 @@ if [[ "$ans" == "i3wm" ]]; then
   ./twostep.sh
 fi
 if [[ "$ans" == "bspwm" ]]; then
-  echo -e "Script will install bspwm dotfiles from git"
+  echo -e "${YELLOW}Script will install bspwm dotfiles from git${NC}"
   git clone https://github.com/auxmeet/bspwm-dotfiles.git 
   cd bspwm-dotfiles
   chmod +x onestep.sh
@@ -19,14 +23,14 @@ if [[ "$ans" == "bspwm" ]]; then
   sudo ./onestep.sh
   ./twostep.sh
 fi
-echo -e "Install grub settings?"
+echo -e "${YELLOW}Install optimized grub settings?${NC}"
 read -p "Continue? (y/n): " ans
 if [[ "$ans" == "y" ]]; then
-git clone https://github.com/auxmeet/grub-nvidia.git 
+git clone https://github.com/auxmeet/grub-cachyos-optimized.git 
 cd grub-nvidia
 chmod +x install.sh
 sudo ./install.sh
 else
-echo -e "Okay! Goodbye"
+echo -e "${GREEN}✓ All set!${NC}"
 exit 0
 fi
